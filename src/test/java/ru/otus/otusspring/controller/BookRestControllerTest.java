a836da4838ce4ef0cceec28e5b9fc515e166a59e
Oleg Sychev <sychevoa@elfin.tech>
2020-08-08 23:57:33 +0300

    test changed

--- src/test/java/ru/otus/otusspring/controller/BookRestControllerTest.java
+++ src/test/java/ru/otus/otusspring/controller/BookRestControllerTest.java
@@ -7,18 +7,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.otus.otusspring.model.Book;
 import ru.otus.otusspring.service.BookService;
 
-import java.util.List;
-
-import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(BookController.class)
-@DisplayName("BookController должен: ")
-public class BookControllerTest {
+@WebMvcTest(BookRestController.class)
+@DisplayName("BookRestController должен: ")
+public class BookRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -27,13 +25,12 @@ public class BookControllerTest {
     private BookService bookService;
 
     @Test
-    @DisplayName("выводить все книги на главной странице")
-    public void test() throws Exception {
-        given(bookService.getAll()).willReturn(List.of(new Book("Anna Karenina", "Leo Tolstoy", "novel", "no comment")));
+    @DisplayName("получать ожидаемую книгу по ID")
+    public void shouldReturnExpectedBookById() throws Exception {
+        given(bookService.getOne("1")).willReturn(new Book("Anna Karenina", "Leo Tolstoy", "novel", "no comment"));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/book/1"))
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("books"))
-                .andExpect(content().string(containsString("Anna Karenina")));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Anna Karenina"));
     }
 }
